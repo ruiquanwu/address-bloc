80cd56164652de4f15e3c9fe69693c0fa04d1418
ruiquan wu <ruiquanwu@gmail.com>
2015-06-05 04:17:15 +0000

    completed assignment-ruby-models

--- models/address_book.rb
+++ models/address_book.rb
@@ -22,17 +22,6 @@ class AddressBook
    end
   
   def remove_entry(entry_to_delete)
-    entry_index = nil
-    @entries.each_with_index do |entry, index|
-      if entry.name == entry_to_delete.name &&
-        entry.phone_number == entry_to_delete.phone_number &&
-        entry.email == entry_to_delete.email
-          entry_index = index
-      end
-    end
-    if entry_index != nil
-      @entries.delete(entry_index)
-    end
-    @entries
+    @entries.delete(entry_to_delete)
   end
 end
